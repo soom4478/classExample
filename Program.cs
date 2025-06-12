81ed3e4980534d1ec17e37c1652c15e360bfd55c
soom <s2305@e-mirim.hs.kr>
2025-06-12 14:06:26 +0900

    상속 시 생성자 예

--- Program.cs
+++ Program.cs
@@ -8,6 +8,18 @@ namespace classExample
 {
     class Program
     {
+        class Parent
+        {
+            public Parent() { Console.WriteLine("Parent()"); }
+            public Parent(int name) { Console.WriteLine("Parent()"); }
+            public Parent(string param) { Console.WriteLine("Parent()"); }
+        }
+         class Child : Parent
+        {
+            public Child() : base(10) { Console.WriteLine("Child():base(int)"); }
+            public Child(string input) : base(input) { Console.WriteLine("Child(input):base(input)"); }
+
+        }
         static void Main(string[] args)
         {
         }
