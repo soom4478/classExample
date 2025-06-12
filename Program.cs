48a5202589abc1a8021cf7c1c2b9a2963e272386
soom <s2305@e-mirim.hs.kr>
2025-06-12 14:47:48 +0900

    하이딩 예(변수, 메서드)

--- Program.cs
+++ Program.cs
@@ -10,6 +10,12 @@ namespace classExample
     {
         class Parent
         {
+            public int variable = 237;
+            public void Method()
+            {
+                Console.WriteLine("부모 메서드");
+            }
+
             public static int counter = 0;
             public void CounterParent()
             {
@@ -18,9 +24,15 @@ namespace classExample
             public Parent() { Console.WriteLine("Parent()"); }
             public Parent(int name) { Console.WriteLine("Parent()"); }
             public Parent(string param) { Console.WriteLine("Parent()"); }
+            
         }
          class Child : Parent
         {
+            public string variable = "하이딩";
+            public void Method()
+            {
+                Console.WriteLine("자식 메서드");
+            }
             public void CountChild()
             {
                 Child.counter++;
@@ -29,8 +41,22 @@ namespace classExample
             public Child(string input) : base(input) { Console.WriteLine("Child(input):base(input)"); }
 
         }
+        public static int number = 10; // 가려진 부모 변수
         static void Main(string[] args)
         {
+            // 섀도잉
+            int number = 10; // 섀도잉 예.number 변수가 지역 변수로 가려짐
+            Console.WriteLine(number);
+            Console.WriteLine(Program.number);
+
+            // 하이딩
+            Child c = new Child();
+            Console.WriteLine(c.variable);
+            Console.WriteLine(((Parent)c).variable);
+            Console.WriteLine((c as Parent).variable);
+            c.Method();
+            ((Parent)c).Method(); // 하이딩 된 부모의 메서드 접근
+
             Child childA = new Child();
             Child childB = new Child("string");
 
